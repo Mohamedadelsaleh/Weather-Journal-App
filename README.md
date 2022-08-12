26ee28cb454e95471c89e9d75402e6d94304a8e6
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-08-12 19:44:32 +0200

    Udacity

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Weather-Journal-App
 Weather Journal App (Udacity)
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/built-by-developers.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/uses-git.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/uses-css.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/powered-by-coffee.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/uses-js.svg)](https://forthebadge.com)
+
+## Table of contents
+* [Setup](#setup)
+* [Purpose](#purpose)
+* [Pictures](#pictures)
+* [Authors](#authors)
+* [Built With](#built-with)
+***
+
+## Setup
+
+To start the project follow this few steps:
+
+1- Setup Nodejs and packages (express, cors and body-parser) to create our server.
+2- In terminal write this command node server.js "Make sure that you in the main file of server.js file"
+
+
+
+## Purpose
+
+  It's a Udacity project You can Join FWD Program and develop it in the way you want .
+  - Work with APIs 
+  - Work with Asynchronous code
+  - Fetching Data and get the specific data that you want
+  - Update UI dynamically
+
+## Pictures
+
+## Authors
+* [Mohamed Adel Salah Gouda](https://github.com/Mohamedadelsaleh)
+
+## Built With
+* HTML5
+* CSS3
+* JavaScript
+* ES6
+* Node.js
---
+++ website/app.js
@@ -0,0 +1,80 @@
+/* Global Variables */
+
+let generateBTN = document.getElementById('generate');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//Weather app API
+
+let websiteURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+let myAPI = '&appid=1f4eb02b39c57d53f2609e7c0fa5094e';
+
+
+generateBTN.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const zipCode = document.getElementById('zip').value;
+    const userFeelings = document.getElementById('feelings').value;
+    cloneWeatherData(websiteURL, zipCode, myAPI).then(
+        (data)=>{
+            postData('/send',{date:d, temp:data.list[0].main.temp, content:userFeelings})
+            .then(()=>{newUI()})
+            .catch((err)=>{
+                console.log(error);
+                alert('The zip code is invalid. Try again');
+            })
+        }
+    )
+});
+
+
+//clone weather data
+const cloneWeatherData = async(websiteURL, zipCode, myAPI)=>{
+    const res = await fetch(websiteURL+zipCode+myAPI)
+    try{
+        const data = await res.json();
+        return data;
+    }catch(err){
+        console.log('we found error that is ', err);
+    }
+}
+
+// Posting Data
+const postData = async (URL='', data ={})=>{
+    const res = await fetch(URL, {
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            temp: data.temp,
+            date: data.date,
+            content: data.content
+        })
+    });
+
+    try{
+        const newDate = await res.json();
+        return newDate;
+    }catch(err){
+        console.log('we found error that is ', err);
+    }
+
+}
+
+//Get Data
+
+const newUI = async ()=>{
+    const req = await fetch('/tempData');
+    try{
+        const tempData = await req.json();
+        console.log(tempData);
+        document.getElementById('date').innerHTML = `Date: ${tempData.date}`;
+        document.getElementById('temp').innerHTML = `Temperature: ${tempData.temp}`;
+        document.getElementById('content').innerHTML = `Feelings: ${tempData.content}`;
+    }catch(err){
+        console.log('we found error that is ', err);
+    }
+}
